5424d2a100b5240e518ec926856629d3c1ee98e9
vaaluvishnu5146 <vaaluvishnu5146@gmail.com>
2024-10-17 20:14:46 +0530

    Products Api code changes

---
+++ index.js
@@ -0,0 +1,90 @@
+const Express = require("express");
+const products = require("./products.json");
+
+// STEP 1: Create a API SERVER
+const API_SERVER = Express();
+
+// PARSING INCOMING REQUEST BODY AS JSON
+API_SERVER.use(Express.json());
+
+// SERVING STATIC FILES
+API_SERVER.use(Express.static('public'));
+
+// ROUTE 1: /
+API_SERVER.get("/", function (request, response) {
+   // return response.send("/ - Request received")
+   return response.status(200).json({
+    message: "Hello Folks",
+    success: true
+   });
+});
+
+// ROUTE 1: /
+API_SERVER.get("/", function (request, response) {
+    // return response.send("/ - Request received")
+    return response.status(200).json({
+     message: "Hello Folks",
+     success: true
+    });
+ });
+
+// ROUTE 2: 
+// PATH = /products
+// METHOD = GET
+API_SERVER.get("/products", function (request, response) {
+    let result = [];
+    const { limit, page } = request.query;
+    if(limit && page) {
+        const start = Number(limit) * (Number(page) - 1) ;
+        const end = Number(limit) * Number(page);
+        result = products.data.slice(start, end);
+    } else {
+        result = products.data;
+    }
+    // return response.send("/ - Request received")
+    return response.status(200).json({
+     message: "Products fetched successfully",
+     success: true,
+     data: result
+    });
+ });
+
+// ROUTE 2: 
+// PATH = /products/{productId}
+// METHOD = GET
+API_SERVER.get("/products/:productId", function (request, response) {
+    // return response.send("/ - Request received"))
+    const matchedProduct = products.data.find((_product) => _product.id === Number(request.params.productId));
+    if (!matchedProduct) {
+        return response.status(404).json({
+            message: "No products found",
+            success: false
+           });    
+    } else {
+        return response.status(200).json({
+            message: "Product fetched successfully",
+            success: true,
+            data: matchedProduct
+           });
+    }
+ });
+
+
+// ROUTE 4:
+// PATH = /products
+// METHOD = POST
+API_SERVER.post("/products/create", function (request, response) {
+    console.log("HERE", request.body);
+    return response.json({
+        message: "Product created successfully!",
+        success: true
+    });
+});
+
+
+
+// STEP 2: START AND LISTEN INCOMING REQUEST TO THIS SERVER
+API_SERVER.listen(3000, "localhost", function () {
+    console.log("Server started");
+    console.log(`http://localhost:3000`)
+});
---
+++ public/js/tailwind.js
@@ -0,0 +1,1 @@
+console.log("Tailwind css");
